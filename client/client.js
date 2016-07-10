31ffa8c56feb0be478c120c721c2f0611d86e052
Alexandre Iooss <alexandre.iooss@sfr.fr>
2016-07-10 20:00:43 +0200

    Respect AI protocol

--- client/client.js
+++ client/client.js
@@ -9,49 +9,28 @@ var rl = readline.createInterface({
 rl.setPrompt('');
 rl.prompt();
 rl.on('line', function(line) {
-    if (line === "quit") {
-      rl.close();
-    } else if (line.startsWith("click")) {
-      args = line.split(" ");
-      x = args[1];
-      y = args[2];
-      socket.emit('click', [x, y]);
-    } else if (line === "reset") {
-      socket.emit('reset');
-    } else if (line.startsWith("level")) {
-      args = line.split(" ");
-      i = args[1];
-      socket.emit('level', i);
-    } else {
-      rl.prompt();
-    }
+  args = line.split(" ");
+  x1 = args[0];
+  y1 = args[1];
+  x2 = args[2];
+  y2 = args[3];
+  socket.emit('click', [x1, y1]);
+  socket.emit('click', [x2, y2]);
 }).on('close',function(){
-    process.exit(0);
+  process.exit(0);
 });
+
+printInfos = function () {
+  table_srt = '';
+  for (line of table) {
+    table_srt += line.join(' ') + ' ';
+  }
+  console.log(turn + ' ' + turn_total + ' ' + score + ' ' + table_srt);
+}
 
 // when server change infos
-socket.on('turn', function(i){
-  console.log('turn ' + i);
-});
-
-socket.on('turn total', function(i){
-  console.log('turn total ' + i);
-});
-
-socket.on('score', function(i){
-  console.log('score ' + i);
-});
-
-socket.on('level', function(i){
-  console.log('level ' + i);
-});
-
-socket.on('selected', function(selected){
-  console.log('selected ' + selected);
-});
-
-// when server change table
-socket.on('table', function(tbl){
-  console.log('table ' + tbl);
-});
+socket.on('turn', function(i){ turn = i; });
+socket.on('turn total', function(i){ turn_total = i; });
+socket.on('score', function(i){ score = i; });
+socket.on('table', function(tbl){ table = tbl; printInfos(); });
 
